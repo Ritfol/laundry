4b7a4a3257cac0db8ca75f26b8f777962fc81efb
Ritfol <zackhaule@gmail.com>
2018-07-25 14:26:04 +0300

    Made some few changes, roll back to the previous commit if anything doesn't work as it should.

--- app/CustomerOrder.php
+++ app/CustomerOrder.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerOrder extends Model
 {
     protected $fillable = ['customer_id' , 'begin' , 'end' , 'package' , 'confirmed'];
+
+
 
     public function customer()
     {
