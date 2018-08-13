ef5e9dbf4574612358b4c31612112a27c9b68607
luckykun88 <kmisigaro@outlook.com>
2018-08-13 17:49:50 +0300

    user UI improvements
    adding code to tender and customer UI in order to improve UI/UX.

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -4,6 +4,63 @@
 
     <div class="container-fluid">
         <strong>Welcome {{ $customer->name }}</strong>
+        <br><br><br>
+          <div class="row">
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-primary o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-shopping-cart"></i>
+              </div>
+              <div class="mr-5">Add new subscription</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="{{ route('customer_toOrder') }}">
+              <span class="float-left">Go</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-warning o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-list"></i>
+              </div>
+              <div class="mr-5">View past subscriptions</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="{{ route('customer_orders') }}">
+              <span class="float-left">Go</span>
+              <span class="float-right">
+              <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+   
+      </div>
+        <br><br><br>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+            <p>
+                As a Quarters Laundry client, you are able to add new subscription for our monthly services. Upon submiting your order,
+                your request will be reviewed, then confirmed. Quarters Laundry personnel will contact you the let you know you are subscibed 
+                for the current period of one month
+            </p><br>
+            <p>
+                Please note that the pickup days will be <b>WEDNESDAYS</b> and <b>SATURDAYS</b> only. With delivery days being <b>MONDAYS</b> and <b>FRIDAYS</b>.
+            </p><br>
+            <p>
+                As a Quarters member client, you will be able to view past subscriptions, as well as check when the current subscription
+                starts and ends.
+            </p>
+            </div>
+            <div class="col-md-2"></div>
+        
+        </div>
+ <!--//--->
     </div>
 
 @endsection
--- resources/views/tender/dashboard.blade.php
+++ resources/views/tender/dashboard.blade.php
@@ -4,6 +4,57 @@
 
     <div class="container-fluid">
         <strong>Logged in as {{ $tender->name }}</strong>
+         <br><br><br>
+          <div class="row">
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-primary o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-shopping-cart"></i>
+              </div>
+              <div class="mr-5">Make new tender subscription</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="{{ route('tender_toOrder') }}">
+              <span class="float-left">Go</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-warning o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-list"></i>
+              </div>
+              <div class="mr-5">View past tender subscriptions</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="{{ route('tender_orders') }}">
+              <span class="float-left">Go</span>
+              <span class="float-right">
+              <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>
+   
+      </div>
+              <br><br><br>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+            <p>
+                Tender clients have accounts so they can keep track of the various tender requests they can make in the future
+            </p><br>
+            <p>
+                You are able to add new tender subscriptions, whereby your qoute will be reviewed, and you will be contacted by
+                Quarters personnel, whereby the terms will be discussed, agreed upon..and the service will be rendered upon agreement.
+            </p>
+            </div>
+            <div class="col-md-2"></div>
+        
+        </div>
     </div>
 
 @endsection
