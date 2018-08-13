bb24d8be69c72953c50653aa5a46d85fe7b806f9
luckykun88 <kmisigaro@outlook.com>
2018-08-13 19:37:20 +0300

    remove public zip

--- resources/views/includes/sitenav.blade.php
+++ resources/views/includes/sitenav.blade.php
@@ -12,7 +12,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="first-list"><a class="active" href="{{ route('homepage') }}">Home</a></li>
+                <li class="first-list"><a href="{{ route('homepage') }}">Home</a></li>
                 <li><a href="{{ route('about') }}">About</a></li>
                 <li><a href="{{ route('services') }}">Services</a></li>
                 <li><a href="{{ route('tender_services') }}">Tender Service</a></li>
